01f282b3db8444b6a63e9b006374d387d1f047d6
Erick da Fonseca Pachi <erickf.pachi@gmail.com>
2020-09-02 18:31:10 +1000

    Updates

---
+++ precache-manifest.8d7179a271df38fe46e984b77a7c5ca3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f47e494142b44348134e41814e3294",
+    "url": "/Notepad/index.html"
+  },
+  {
+    "revision": "b9314d6686ffee8b6843",
+    "url": "/Notepad/static/css/2.cbd386e9.chunk.css"
+  },
+  {
+    "revision": "5e865049a7ef6ce4f0ec",
+    "url": "/Notepad/static/css/main.cf11adbe.chunk.css"
+  },
+  {
+    "revision": "b9314d6686ffee8b6843",
+    "url": "/Notepad/static/js/2.d7f7db9f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Notepad/static/js/2.d7f7db9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e865049a7ef6ce4f0ec",
+    "url": "/Notepad/static/js/main.77be5916.chunk.js"
+  },
+  {
+    "revision": "ab51950e6a8b76d0b156",
+    "url": "/Notepad/static/js/runtime-main.b9dfaa7f.js"
+  }
+]);
